b7e0d562b6e6a8dc24c698af6b26a0f701d5b691
AleNavarro <ale.naengram@gmail.com>
2024-05-30 11:00:08 +0200

    creacionComentarios

---
+++ pages/comentarios.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Compu - Comparador PHP</title>
+</head>
+<body>
+
+    <div class="fondo"><img src="img/fondo.jpg" alt="fondo">
+
+        <div class="rectanculoFondo">
+
+            <h1>Comentarios</h1>
+
+            <textarea id="comentario" placeholder="Escribe tu comentario aquí..." max-length="200"></textarea>
+
+            <br>
+
+            <div class=cajaBoton>
+                
+                <button id="enviar">Enviar</button>
+
+            </div>
+
+        </div>
+    
+    </div>
+
+</body>
+</html>
